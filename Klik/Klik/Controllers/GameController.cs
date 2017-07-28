8e785d97365f14cbf9e98c097189713a21c8b330
Siong <siong.esteban@yahoo.com>
2017-07-28 23:47:40 +0800

    Move Players to GameController

--- Klik/Klik/Controllers/GameController.cs
+++ Klik/Klik/Controllers/GameController.cs
@@ -14,6 +14,11 @@ namespace Klik.Controllers
             return View();
         }
 
+        public ActionResult Players()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
