94f0e8a90e48528a0d23d396e356985c5116e83d
Siong <siong.esteban@yahoo.com>
2017-07-29 10:10:59 +0800

    Add PlayerViewModel and queries

--- Klik/Klik/Controllers/GameController.cs
+++ Klik/Klik/Controllers/GameController.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Klik.Models;
+using Klik.ViewModels;
 
 namespace Klik.Controllers
 {
     public class GameController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GameController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -18,7 +32,15 @@ namespace Klik.Controllers
         [Route("players")]
         public ActionResult Players()
         {
-            return View();
+            var gameSessions = _context.GameSessions.Include(gs => gs.User).ToList();
+            var players = _context.Users.ToList();
+            var viewModel = new PlayerViewModel
+            {
+                Player = players,
+                GameSession = gameSessions
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Klik/Klik/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Klik.Models;
+
+namespace Klik.ViewModels
+{
+    public class PlayerViewModel
+    {
+        public IEnumerable<ApplicationUser> Player { get; set; }
+        public IEnumerable<GameSession> GameSession { get; set; }
+    }
+}
